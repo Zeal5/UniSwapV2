4cabaaefcc90fcec2d27dac0e4734d4ef6186984
Zeal5 <tassadiqshakeel5@gmail.com>
2023-05-25 14:56:12 +0500

    setting envirnmental vcariables

--- scripts/deployeFactory.py
+++ scripts/deployeFactory.py
@@ -1,4 +1,18 @@
+from brownie import accounts, config, UniswapV2Factory
+
+
+def deploye_factory():
+    private_key = config["wallets"]["from_key"]
+    owner_account = accounts.add(private_key)
+    account = accounts[0]
+    factory = UniswapV2Factory.deploy(owner_account, {"from": account} )
+    init_hash = factory.
+
+    # Deploye a contract
+    # Import a user wallet
+    # private_key = config["wallets"]["from_key"]
+    # account = accounts.add(private_key)
 
 
 def main():
-    print("hello")
+    deploye_factory()
