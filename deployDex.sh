b5854628a15eb546f2e51f35560ad3f8eee3b9e4
Zeal5 <tassadiqshakeel5@gmail.com>
2023-08-03 08:41:10 +0500

    .exp to fully automate factory/router interaction

--- deployDex.sh
+++ deployDex.sh
@@ -38,3 +38,45 @@ echo "approving tokens"
 cd ../erc_tokens
 brownie run scripts/approveToken.py
 echo "done"
+
+echo "creating Uniswap pairs"
+cd ../IP_Router
+# load all variables before opening brownie console
+factory_address=$(jq -r '.factory_address' ../factory_data.json)
+router_address=$(jq -r '.router_address' ../router_data.json)
+t1_address=$(jq -r '.t1_address' ../token_data.json)
+t2_address=$(jq -r '.t2_address' ../token_data.json)
+echo "router address: $router_address"
+echo "t1 address: $t1_address"
+echo "t2 address: $t2_address"
+
+
+router_commands="brownie_commands.exp $router_address $t1_address $t2_address"
+factory_commands="../IP_core/brownie_commands.exp $factory_address $t1_address $t2_address"
+
+gnome-terminal --working-directory="/home/zeal/UniswapFork/IP_Router" -- bash -c "expect $router_commands; exec bash" &
+gnome-terminal --working-directory="/home/zeal/UniswapFork/IP_core" -- bash -c "expect $factory_commands; exec bash"
+
+
+# expect brownie_commands.exp $router_address $t1_address $t2_address
+
+
+
+
+
+
+# brownie console
+# echo "
+# router = Contract.from_abi('router', '$router_address', UniswapV2Router02.abi, owner=accounts[0])
+# t1 = Contract.from_abi('t1', '$t1_address', ERC20.abi, owner=accounts[0])
+# " | brownie console
+
+
+
+# router = Contract.from_abi("router",router_address,UniswapV2Router02.abi,owner=accounts[0])
+# t1 = Contract.from_abi("t1",t1_address,ERC20.abi,owner=accounts[0])
+# t2 = Contract.from_abi("t2",t2_address,ERC20.abi,owner=accounts[0])
+# echo "checking allowance for t1 / router"
+# t1.allowance(accounts[0],router.address)
+# echo "checking allowance for t2 / router"
+# t2.allowance(accounts[0],router.address)
